fcada1e926be61a4ea808a6e87e5cfc033475c03
Richard Miller <richardmmill@gmail.com>
2021-02-28 18:09:50 -0500

    Richar's work on 2.3 code-along

---
+++ index.js
@@ -0,0 +1,35 @@
+// console.log('test connect')
+
+let count = 0
+
+function increase() {
+    count += 1;
+    console.log(count);
+    element.innerText = count;
+};
+
+function decrease() {
+    count -= 1;
+    console.log(count);
+    element.innerText = count;
+};
+
+function reset() {
+    count = 0;
+    console.log(count);
+    element.innerHTML = '<mark>' + count + '</mark>';
+    // element.innerText = count;
+};
+
+const element = document.getElementById('counter');
+
+function selectTheme(theme) {
+    document.getElementsByTagName('body')[0].className = theme;
+    document.getElementsByTagName('main')[0].className = theme;
+    const buttons = document.getElementsByTagName('button');
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].className = theme;
+    };
+};
+
